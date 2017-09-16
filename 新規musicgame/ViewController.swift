d72ec17c0ec2e4cf453cac9ca7e283201cdd9fb4
EriyaMurakami <harumakikazuto@gmail.com>
2017-09-16 15:21:31 +0900

    Initial Commit

---
+++ 新規musicgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  新規musicgame
+//
+//  Created by EriyaMurakami on 2017/09/16.
+//  Copyright © 2017年 EriyaMurakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
